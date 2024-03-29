7c2ccaae39723b89c3a46aa029b33ff3fd47708d
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-03-28 21:09:18 -0300

    add functions shopping cart and other funtions

--- my-shopping-app/src/App.js
+++ my-shopping-app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
 import './App.css';
+import Product from './components/Product';
+import ShoppingCart from './components/ShoppingCart';
+import { getProducts } from './services/productService';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="product-list">
+        <h2>Products</h2>
+        {getProducts().map((product, index) => (
+          <Product key={index} product={product} onAddToCart={handleAddToCart} />
+        ))}
+      </div>
+      <ShoppingCart cartItems={cartItems} />
     </div>
   );
 }
