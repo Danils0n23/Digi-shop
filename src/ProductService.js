a9e47986cf96d334a925144d29cfe95c231f0761
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-03-28 21:54:13 -0300

    primeira etapa concluida

--- src/ProductService.js
+++ src/ProductService.js
@@ -1,5 +1,5 @@
 // src/services/productService.js
-import productsData from '../data/products.json';
+import productsData from './data/products.json';
 
 export const getProducts = () => {
   return productsData;
