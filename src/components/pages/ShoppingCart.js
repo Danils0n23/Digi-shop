cce74a3b28a082d1652dcb2f7848b92f4ad2a723
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-03-28 23:33:09 -0300

    ajustes

--- src/components/pages/ShoppingCart.js
+++ src/components/pages/ShoppingCart.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import Modal from '../modal/modal';
-import './Shopping.css';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import Modal from "../modal/modal";
+import "./Shopping.css";
 
 const ShoppingCart = ({ cartItems }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+  const totalItems = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
 
   return (
     <div className="shopping-cart">
-     <button className="cart-button" onClick={() => setIsModalOpen(true)}>
+      <button className="cart-button" onClick={() => setIsModalOpen(true)}>
         <FontAwesomeIcon icon={faShoppingCart} />
-        <span className="badge">{totalItems}</span>
       </button>
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <h2>Shopping Cart</h2>
