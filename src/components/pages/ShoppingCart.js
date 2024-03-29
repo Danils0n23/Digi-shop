e035525f6d74f995820621f2e818ce1e71650bb2
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-03-28 23:18:07 -0300

    alterações de estilização

--- src/components/pages/ShoppingCart.js
+++ src/components/pages/ShoppingCart.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importe o componente FontAwesomeIcon
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'; // Importe o ícone específico do carrinho de compras do FontAwesome
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import Modal from '../modal/modal';
 import './Shopping.css';
 
 const ShoppingCart = ({ cartItems }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
 
   return (
     <div className="shopping-cart">
-      <h2>Shopping Cart <FontAwesomeIcon icon={faShoppingCart} /></h2> {/* Use FontAwesomeIcon para adicionar o ícone do carrinho */}
-      <p>Total Items: {totalItems}</p>
-      <ul>
-        {cartItems.map((item, index) => (
-          <li key={index}>
-            <div>
-              <span>{item.name}</span>
-              <span>${item.price}</span>
-              <span>Quantity: {item.quantity}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
+     <button className="cart-button" onClick={() => setIsModalOpen(true)}>
+        <FontAwesomeIcon icon={faShoppingCart} />
+        <span className="badge">{totalItems}</span>
+      </button>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <h2>Shopping Cart</h2>
+        <p>Total Items: {totalItems}</p>
+        <ul>
+          {cartItems.map((item, index) => (
+            <li key={index}>
+              <div>
+                <span>{item.name}</span>
+                <span>${item.price}</span>
+                <span>Quantity: {item.quantity}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Modal>
     </div>
   );
 };
