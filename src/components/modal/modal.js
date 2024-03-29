bafd425f0c42da7a1721394820a7244402200c70
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-03-29 05:21:39 -0300

    ajustes finais

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Modal.css';
 
 const Modal = ({ isOpen, onClose, children }) => {
-  if (!isOpen) return null;
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsActive(true); // Ativa o modal quando ele for aberto
+    } else {
+      setTimeout(() => setIsActive(false), 300); // Desativa o modal após a animação de fechamento
+    }
+  }, [isOpen]);
+
+  if (!isActive) return null;
 
   return (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        {children}
+      <div className="modal-content active" onClick={(e) => e.stopPropagation()}>
+        <div className="modal-header">
+          <button className="close-button" onClick={onClose}>X</button>
+        </div>
+        <div className="modal-body">
+          {children}
+        </div>
       </div>
     </div>
   );
--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -9,8 +9,11 @@ const Product = ({ product, onAddToCart }) => {
       <div className="product bg-white rounded-lg shadow-md p-4 m-2 md:w-1/2 lg:w-1/3 xl:w-1/4">
         <img src={product.image} alt={product.name} className="w-full h-auto mb-4 rounded" style={{ maxWidth: '100%' }} />
         <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
+        {product.hero && <p className="text-green-500 font-semibold mb-2">{product.hero}</p>}
         <p className="text-sm mb-2">{product.detail}</p>
         <p className="text-gray-700 mb-2">Price: ${product.price}</p>
+        {product.info && <p className="text-gray-700 mb-2">{product.info}</p>}
+        {product.offer && <p className="text-red-500 font-semibold mb-2">Offer: {product.offer}</p>}
         <button onClick={() => onAddToCart(product)} className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
           <FontAwesomeIcon icon={faShoppingCart} className="mr-2" />
           Add to Cart
