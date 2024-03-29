6a3c375c51b1a6b5feb75c24bb6d69bcf807530b
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-03-28 22:29:07 -0300

    alterações

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -1,14 +1,16 @@
-// src/components/Product.js
 import React from 'react';
+import './Product.css';
 
 const Product = ({ product, onAddToCart }) => {
   return (
-    <div className="product">
-      <img src={product.image} alt={product.name} />
-      <h3>{product.name}</h3>
-      <p>{product.detail}</p>
-      <p>Price: ${product.price}</p>
-      <button onClick={() => onAddToCart(product)}>Add to Cart</button>
+    <div className="product bg-white rounded-lg shadow-md p-4">
+      <img src={product.image} alt={product.name} className="w-full h-auto mb-4 rounded" />
+      <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
+      <p className="text-sm mb-2">{product.detail}</p>
+      <p className="text-gray-700 mb-2">Price: ${product.price}</p>
+      <button onClick={() => onAddToCart(product)} className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+        Add to Cart
+      </button>
     </div>
   );
 };
--- src/components/pages/ShoppingCart.js
+++ src/components/pages/ShoppingCart.js
@@ -1,14 +1,23 @@
-// src/components/ShoppingCart.js
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importe o componente FontAwesomeIcon
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'; // Importe o ícone específico do carrinho de compras do FontAwesome
+import './Shopping.css';
 
 const ShoppingCart = ({ cartItems }) => {
+  const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <div className="shopping-cart">
-      <h2>Shopping Cart</h2>
+      <h2>Shopping Cart <FontAwesomeIcon icon={faShoppingCart} /></h2> {/* Use FontAwesomeIcon para adicionar o ícone do carrinho */}
+      <p>Total Items: {totalItems}</p>
       <ul>
         {cartItems.map((item, index) => (
           <li key={index}>
-            {item.name} - ${item.price}
+            <div>
+              <span>{item.name}</span>
+              <span>${item.price}</span>
+              <span>Quantity: {item.quantity}</span>
+            </div>
           </li>
         ))}
       </ul>
